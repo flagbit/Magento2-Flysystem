3b56ce3556b25be1813f59873f66d6ae9acd60df
Robin Homberg <robin.homberg@flagbit.de>
2019-02-14 15:06:30 +0100

    [FLAGBIT-4646] Compatibility changes for magento 2.3.0

--- view/adminhtml/web/js/cms/mediabrowserExtension.js
+++ view/adminhtml/web/js/cms/mediabrowserExtension.js
@@ -7,11 +7,19 @@ define([
             newButton.title = data.flysystemButtonTitle;
             newButton.type = 'button';
             newButton.innerHTML = '<span>' + data.flysystemButtonTitle + '</span>';
+            newButton.classList.add('action-default');
+            newButton.classList.add('scalable');
+            newButton.classList.add('action-quaternary');
             newButton.on('click', function () {
                 MediabrowserUtility.openDialog(data.flysystemButtonUrl)
             });
 
-            $('#modal_dialog_message').find('.insert-actions').first().prepend(newButton);
+
+            if($('#modal_dialog_message').find('.page-action-buttons')) {
+                $('#modal_dialog_message').find('.page-action-buttons').first().prepend(newButton);
+            } else if($('#modal_dialog_message').find('.insert-actions')) {
+                $('#modal_dialog_message').find('.insert-actions').first().prepend(newButton);
+            }
         }
     }
 });
